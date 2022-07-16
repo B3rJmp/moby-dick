371c069007d0e0270fb3c2bea947ac873a45aa89
ThaneStevens <thanerstevens@gmail.com>
2022-07-15 23:05:12 -0600

    finished basic functionality

---
+++ moby-dick-vanilla-js/index.js
@@ -0,0 +1,50 @@
+const {promises: fsPromises} = require('fs');
+
+async function asyncReadFile(filename) {
+  try {
+    const contents = await fsPromises.readFile(filename, 'utf-8');
+    const stopWordFile = await fsPromises.readFile('stop-words.txt', 'utf-8');
+
+    const lines = contents.split(/\r?\n/);
+    const stopWords = stopWordFile.split(/\r?\n/);
+
+    var words = []
+    var counted = []
+
+    for(line of lines) {
+      let wordsInLine = line.split(' ').map(l => l.replace(/[^a-zA-Z0-9 ]/g, '').toLowerCase())
+      wordsInLine.forEach(w => {
+        if(w && w !== '' && !stopWords.includes(w)) {
+          words.push(w)
+        }
+      })
+    }
+
+    words.forEach(w => {
+      if(counted.find(c => c.word === w)) {
+        counted[counted.findIndex(c => c.word === w)].count++
+      }else{
+        let newWord = {
+          word: w,
+          count: 1
+        }
+        counted.push(newWord)
+      }
+    })
+
+    counted.sort((a,b) => {
+      return a.count > b.count ? -1 : a.count < b.count ? 1 : 0
+    })
+
+    if (counted.length > 100) {
+      return console.log(counted.slice(0,99))
+    } else {
+      return console.log(counted)
+    }
+  } catch (err) {
+    console.log(err);
+  }
+
+}
+
+asyncReadFile('./moby-dick.txt');
