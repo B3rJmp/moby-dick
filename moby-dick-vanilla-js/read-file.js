1f1f57e4c921f51d0dfe9c5280a360cad95e9e48
ThaneStevens <thanerstevens@gmail.com>
2022-07-16 13:41:43 -0600

    cleaned up vanilla-js app

--- moby-dick-vanilla-js/read-file.js
+++ moby-dick-vanilla-js/read-file.js
@@ -1,15 +1,9 @@
 const {promises: fsPromises} = require('fs');
 
-// function createBar(word) {
-//   const target = document.getElementById('words')
-//   const width = (word.count/topWordCount).toFixed(1)
-//   console.log(27,width)
-// }
-
 async function readFile() {
   try {
-    const contents = await fsPromises.readFile('text-files/moby-dick.txt', 'utf-8');
-    const stopWordFile = await fsPromises.readFile('text-files/stop-words.txt', 'utf-8');
+    const contents = await fsPromises.readFile('./text-files/moby-dick.txt', 'utf-8');
+    const stopWordFile = await fsPromises.readFile('./text-files/stop-words.txt', 'utf-8');
 
     const lines = contents.split(/\r?\n/);
     const stopWords = stopWordFile.split(/\r?\n/);
@@ -17,9 +11,9 @@ async function readFile() {
     var words = []
     var counted = []
 
-    lines.forEach(line => {
+    for(let line of lines) {
       let wordsInLine = line.split(' ').map(l => l.replace(/[^a-zA-Z0-9 ]/g, '').toLowerCase())
-      wordsInLine.forEach(w => {
+      for(let w of wordsInLine) {
         if(w && w !== '' && !stopWords.includes(w)) {
           let index = counted.findIndex(c => c.word === w)
           if(index != -1) {
@@ -32,8 +26,8 @@ async function readFile() {
             counted.push(newWord)
           }
         }
-      })
-    })
+      }
+    }
 
     counted.sort((a,b) => {
       return a.count > b.count ? -1 : a.count < b.count ? 1 : 0
