3e64954c146b422a3dbe388cef6a1154ec53372d
ThaneStevens <thanerstevens@gmail.com>
2022-07-16 13:41:27 -0600

    started basic php app

---
+++ moby-dick-php/js/read-file.js
@@ -0,0 +1,52 @@
+const {promises: fsPromises} = require('fs');
+
+// function createBar(word) {
+//   const target = document.getElementById('words')
+//   const width = (word.count/topWordCount).toFixed(1)
+//   console.log(27,width)
+// }
+
+async function readFile() {
+  try {
+    const contents = await fsPromises.readFile('text-files/moby-dick.txt', 'utf-8');
+    const stopWordFile = await fsPromises.readFile('text-files/stop-words.txt', 'utf-8');
+
+    const lines = contents.split(/\r?\n/);
+    const stopWords = stopWordFile.split(/\r?\n/);
+
+    var words = []
+    var counted = []
+
+    lines.forEach(line => {
+      let wordsInLine = line.split(' ').map(l => l.replace(/[^a-zA-Z0-9 ]/g, '').toLowerCase())
+      wordsInLine.forEach(w => {
+        if(w && w !== '' && !stopWords.includes(w)) {
+          let index = counted.findIndex(c => c.word === w)
+          if(index != -1) {
+            counted[index].count++
+          }else{
+            let newWord = {
+              word: w,
+              count: 1
+            }
+            counted.push(newWord)
+          }
+        }
+      })
+    })
+
+    counted.sort((a,b) => {
+      return a.count > b.count ? -1 : a.count < b.count ? 1 : 0
+    })
+
+    return counted.slice(0,100)
+
+  } catch (err) {
+    console.error(err);
+  }
+
+}
+
+readFile()
+  .then(res => console.log(res))
+  .catch(err => err)
