7d987ea37fc06a04ebdef4375ec0f1396e381e01
ThaneStevens <thanerstevens@gmail.com>
2022-07-17 19:33:31 -0600

    edited titles for each app

--- moby-dick-php/public/index.php
+++ moby-dick-php/public/index.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Moby Dick (vanilla js)</title>
+    <title>Moby Dick (PHP)</title>
     <link rel="stylesheet" href="css/index.css">
   </head>
   <body>
